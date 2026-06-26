5adba8b285a1d4948a51e1e4c37f9bf1aa56dfd3
Dimpho Mouba <ST10492572@rcconnect.edu.za>
2026-06-26 03:35:40 +0200

    Add Activity Log feature with show more option

---
+++ CybersecurityAwarenessBotGUI/CybersecurityAwarenessBotGUI/ActivityLog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CybersecurityAwarenessBotGUI
+{
+    public static class ActivityLog
+    {
+        private static List<string> _log = new List<string>();
+
+        public static void Add(string action)
+        {
+            string entry = $"[{DateTime.Now:HH:mm:ss}] {action}";
+            _log.Add(entry);
+        }
+
+        public static List<string> GetRecent(int count = 10)
+        {
+            var recent = _log.Skip(Math.Max(0, _log.Count - count)).ToList();
+            recent.Reverse();
+            return recent;
+        }
+
+        public static List<string> GetAll()
+        {
+            var all = _log.ToList();
+            all.Reverse();
+            return all;
+        }
+
+        public static int TotalCount => _log.Count;
+    }
+}
---
+++ CybersecurityAwarenessBotGUI/CybersecurityAwarenessBotGUI/ActivityLogWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace CybersecurityAwarenessBotGUI
+{
+    public partial class ActivityLogWindow : Window
+    {
+        private bool _showingAll = false;
+
+        public ActivityLogWindow()
+        {
+            InitializeComponent();
+            LoadLog();
+        }
+
+        private void LoadLog()
+        {
+            if (_showingAll)
+            {
+                LogListBox.ItemsSource = ActivityLog.GetAll();
+                SubtitleText.Text = $"Showing all {ActivityLog.TotalCount} actions";
+                ShowMoreButton.Content = "▲ SHOW LESS";
+            }
+            else
+            {
+                LogListBox.ItemsSource = ActivityLog.GetRecent(10);
+                SubtitleText.Text = $"Showing last 10 of {ActivityLog.TotalCount} actions";
+                ShowMoreButton.Content = "▼ SHOW FULL HISTORY";
+            }
+
+            if (ActivityLog.TotalCount == 0)
+            {
+                LogListBox.ItemsSource = new[] { "No activity recorded yet." };
+                ShowMoreButton.IsEnabled = false;
+            }
+        }
+
+        private void ShowMore_Click(object sender, RoutedEventArgs e)
+        {
+            _showingAll = !_showingAll;
+            LoadLog();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
